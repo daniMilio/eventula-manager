11ccb712598876fa5e01732c9eba99f1c045110c
dani <11715018+daniMilio@users.noreply.github.com>
2025-01-04 23:18:19 +0100

    fix url

--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@ return [
 
     'env'               => env('APP_ENV', 'production'),
     'debug'             => env('APP_DEBUG', false),
-    'url'               => env('APP_URL', 'localhost'),
+    'url'               => env('APP_URL', ''),
     'timezone'          => 'UTC',
     'locale'            => 'en',
     'fallback_locale'   => 'en',
